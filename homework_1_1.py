22c60aef1b903ee94fd94740f3ae7cbcd3a952aa
Matsievskiy <ian.m@mail.ru>
2025-03-12 22:57:39 +0500

    Add files via upload

---
+++ homework_1_1.py
@@ -0,0 +1,8 @@
+'''Задача 1. Азы азов'''
+
+product_price_1 = input('Введите цену товара №1: ')
+product_price_2 = input('Введите цену товара №2: ')
+
+summa = float(product_price_1) + float(product_price_2)
+
+print('Общая сумма:', summa)
